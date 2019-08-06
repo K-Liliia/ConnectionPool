6c3508b2f22aa66bd5847d99fd30a254eb131391
K-Liliia <vodoliy96@gmail.com>
2019-08-06 17:56:59 +0300

    New main class

--- ConnectionPool/src/com/solvd/connectionPool/Main.java
+++ ConnectionPool/src/com/solvd/connectionPool/Main.java
@@ -1,9 +1,8 @@
 package com.solvd.connectionPool;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import org.apache.log4j.Logger;
@@ -22,22 +21,19 @@ public class Main {
 //parallel thread, don't return result - <Void> with runAsync() method
 		CompletableFuture<Void> future = CompletableFuture.runAsync(new Runnable() {
 
-			List<Connection> connections = new ArrayList<>();
-
 			@Override
 			public void run() {
-				for (int i = 0; i < 10; i++) {
-					try {
-						connections.add(myPool.getConnection());
-					} catch (InterruptedException e) {
-						// logger.info("InterruptedException");
-						System.out.println("InterruptedException");
-					} catch (SQLException e) {
-						// logger.info("SQLException");
-						System.out.println("SQLException");
-					}
+
+				try {
+					Connection connection = DriverManager.getConnection(
+							"jdbc:mysql://52.59.193.212:3306/Railway_station?serverTimezone=Europe/Kiev&useSSL=false",
+							"root", "devintern");
+				} catch (SQLException e) {
+					// logger.info("SQLException");
+					System.out.println("SQLException");
 				}
 			}
+
 		});
 
 		future.get(); // blocks the current thread until the result is ready
